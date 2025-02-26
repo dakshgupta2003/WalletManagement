3173987dce7f8300621dc96e684ef0c704cf601e
dakshgupta <t-daksh.gupta@ocltp.com>
2025-02-26 11:15:18 +0530

    unnecessary files removed

--- src/main/java/com/payment/wallet/config/JacksonConfig.java
+++ src/main/java/com/payment/wallet/config/JacksonConfig.java
@@ -8,7 +8,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfig {
-    
+
+    // purpose of this class is to configure the Jackson ObjectMapper to serialize and deserialize dates in a specific format
+    // this is to ensure that the dates are serialized in the format "2024-01-01T00:00:00.000Z" and deserialized in the format "2024-01-01T00:00:00.000Z"
+    // Prevents dates from being written as arrays/timestamps [2024, 3, 15, 10, 30]
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
